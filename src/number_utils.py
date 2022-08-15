4e8fc13de0feb47e5f23583af09184b37041ceff
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-08-15 15:41:42 +0700

    fix: 🐛 update is prime function
    to handle number that less or equal than one

--- src/number_utils.py
+++ src/number_utils.py
@@ -1,5 +1,7 @@
 def is_prime_list(numbers):
     for num in numbers:
+        if num <= 1:
+            return False
         for n in range(2, num):
             if num % n == 0:
                 return False
