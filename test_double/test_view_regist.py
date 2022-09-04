bcf26cebababc89c353d6ba60a0ca8139b5ec48b
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-09-04 16:36:15 +0700

    add view regist category of test_double

---
+++ test_double/test_view_regist.py
@@ -0,0 +1,36 @@
+import unittest
+import unittest.mock as mock
+import view_regist
+
+# how to test
+# cd test_double
+# nose2 -v --with-coverage --coverage view_regist test_view_regist
+
+
+class ViewRegistryTest(unittest.TestCase):
+    def test_no_view_provided(self):
+        result = view_regist.get_subblueprints()
+        self.assertEqual(result, [])
+
+    @mock.patch("view_regist.dir", return_value=[])
+    def test_one_view_no_subviews(self, mock_dir):
+        views = [mock.MagicMock()]
+        views[0].module = "first"
+        # print(views[0].module)
+        result = view_regist.get_subblueprints(views)
+
+        mock_dir.assert_called_once()
+        self.assertEqual(result, ["first"])
+
+    def test_one_view_one_sub(self):
+        first = mock.MagicMock()
+        second = mock.MagicMock()
+        first.module = mock.MagicMock()
+        first.module.url_prefix = "/first"
+        first.subviews = [second]
+        second.module = mock.MagicMock()
+        second.module.url_prefix = "/second"
+        views = [first]
+        result = view_regist.get_subblueprints(views)
+
+        self.assertEqual(result, [first.module, second.module])
---
+++ test_double/view_regist.py
@@ -0,0 +1,10 @@
+def get_subblueprints(views=[]):
+    blueprints = []
+    for view in views:
+        blueprints.append(view.module)
+        if "subviews" in dir(view):
+            for module in get_subblueprints(view.subviews):
+                if view.module.url_prefix and module.url_prefix:
+                    module.url_prefix = view.module.url_prefix + module.url_prefix
+                blueprints.append(module)
+    return blueprints
